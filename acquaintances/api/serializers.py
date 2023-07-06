9c08c9709c1ba5cb3c6f39f4e39874c89f3b362a
Григорьев Олег <oleg.gregorev.2003@mail.ru>
2023-07-07 01:25:34 +0500

    change model User and create Registration

---
+++ acquaintances/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegistrSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'sex', 'password', 'password2', 'avatar']
+
+    def save(self, *args, **kwargs):
+        user = User(email=self.validated_data['email'])
+        user.sex = self.validated_data['sex']
+        user.first_name = self.validated_data['first_name']
+        user.last_name = self.validated_data['last_name']
+        user.avatar = self.validated_data['avatar']
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError('passwords are not equal!')
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'sex', 'avatar', 'liked_list']
--- acquaintances/api/views.py
+++ acquaintances/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import UserRegistrSerializer
+    
 
-# Create your views here.
+class RegistrUserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegistrSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data['response'] = True
+            return Response(data, status=status.HTTP_200_OK)
+        else: 
+            data = serializer.errors
+            return Response(data)
+
