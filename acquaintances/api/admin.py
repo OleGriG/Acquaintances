5b75c0f8a237c3731af33c36bda102a114280ca7
Григорьев Олег <oleg.gregorev.2003@mail.ru>
2023-07-07 03:28:38 +0500

    create match

--- acquaintances/api/admin.py
+++ acquaintances/api/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Like
 
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Like)
 
--- acquaintances/api/urls.py
+++ acquaintances/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegistrUserView
+from .views import RegistrUserView, match
 
 app_name = 'api'
 
 urlpatterns = [
     path('clients/create/', RegistrUserView.as_view(), name='registr'),
+    path('clients/<int:pk>/match/', match, name='match'),
 ]
